9de6e461828c936d11981319e385373fc9da54fb
jingedawang <wjg172184@163.com>
2017-06-02 11:57:38 +0800

    First version.

---
+++ cmake-build-debug/CMakeFiles/G2OCurveFitting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/G2OCurveFitting.dir/main.cpp.o"
+  "G2OCurveFitting.pdb"
+  "G2OCurveFitting"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/G2OCurveFitting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
